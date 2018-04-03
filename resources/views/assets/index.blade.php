59fc0f554496a421119fa3b34d6f292116bcdbbc
Brett Wilson <brettwilson7@yahoo.com>
2018-04-02 21:45:05 -0400

    Assets index page styling. Moved add asset btn.

--- resources/views/assets/index.blade.php
+++ resources/views/assets/index.blade.php
@@ -30,7 +30,7 @@
 					</div>
 				</th>
 				<th class="sortable">
-					<span>Purchase Date</span>
+					<span>Purchased</span>
 					<div class="sort-wrapper">
 						<a href="/assets?sort=purchase_date&order=asc"><i class="fas fa-caret-up"></i></a>
 						<a href="/assets?sort=purchase_date&order=desc"><i class="fas fa-caret-down"></i></a>
@@ -43,16 +43,19 @@
 		<tbody>
 			@foreach($assets as $asset)
 				<tr>
-					<td>{{ $loop->iteration }}</td>
-					<td><a href="/assets/{{ $asset->id }}">{{ $asset->name }}</a></td>
-					<td>{{ $asset->category }}</td>
+					<td class="asset-number">{{ $loop->iteration }}</td>
+					<td class="asset-name"><a href="/assets/{{ $asset->id }}">{{ $asset->name }}</a></td>
+					<td class="asset-category">{{ $asset->category }}</td>
 					<td class="asset-amount">{{ number_format($asset->amount, 2) }}</td>
-					<td>{{ date_create($asset->purchase_date)->format('M j, Y') }}</td>
+					<td class="asset-purchase-date">{{ date_create($asset->purchase_date)->format('M j, Y') }}</td>
 					<td class="asset-details-btn"><a href="/assets/{{ $asset->id }}">View</a></td>
-					<td class="edit-asset-btn"><a href="/assets/{{ $asset->id }}/edit">Edit</a></td>
+					<td class="asset-edit-btn"><a href="/assets/{{ $asset->id }}/edit">Edit</a></td>
 				</tr>
 			@endforeach
 		</tbody>
 	</table>
+	<div class="add-asset-btn">
+		<a href="/assets/create"><i class="fas fa-plus-circle"></i> Asset</a>
+	</div>
 	{{ $assets->links() }}
 @stop
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,22 +5,24 @@
 
 	</head>
 	<body class="{{ empty($body_class) ? "homepage" : $body_class }}">
+		<header>
+			@if(empty($page_title))
+				@php ($page_title = 'Homepage')
+			@endif
+			<h1 class="page-title">{{ $page_title }}</h1>
+			<menu class="main-menu">
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/assets">Assets</a></li>
+					<li><a href="/">Categories</a></li>
+					<li><a href="/">Reports</a></li>
+				</ul>
+			</menu>
+		</header>
 		<div class="content-wrapper">
-			<header>
-				@if(empty($page_title))
-					@php ($page_title = 'Homepage')
-				@endif
-				<h1 class="page-title">{{ $page_title }}</h1>
-				<menu class="main-menu">
-					<ul>
-						<li><a href="/">Home</a></li>
-						<li><a href="/assets">Assets</a></li>
-						<li><a href="/assets/create">Add Asset</a></li>
-					</ul>
-				</menu>
-			</header>
 			@yield('content')
 		</div>
+		<footer>This is a footer</footer>
 		<script type="text/javascript" src="/js/app.js"></script>
 	</body>
 </html>
